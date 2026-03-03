8995383e721ef7938e65435932acbdb2ece470f7
sergeydorof <virtualgamer191@gmail.com>
2026-03-03 10:45:42 +0700

    Add files via upload

---
+++ lab01/До паттерна/MatchaTea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenFactoryApp.Classes
+{
+    internal class MatchaTea : Drink
+    {
+        private string description = "зеленый чай матча";
+
+        public override string GetDescription() { return description; }
+    }
+}
---
+++ lab01/До паттерна/Sushi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenFactoryApp.Classes
+{
+    internal class Sushi : MainCourse
+    {
+        private string description = "сет суши";
+
+        public override string GetDescription() { return description; }
+    }
+}
